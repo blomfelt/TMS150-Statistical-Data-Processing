0269405091cec52498baff8af4c3bda17d2ff0e0
feloxes <155644281+feloxes@users.noreply.github.com>
2024-09-11 20:55:48 +0200

    Exercise 4 done

--- AssignmentA1/AssignmentA1.R
+++ AssignmentA1/AssignmentA1.R
@@ -4,6 +4,9 @@ setwd("~/Documents/RStudio/TMS150 - Stochastic Data Processing/AssignmentA1")
 
 # To save all plots change to TRUE
 savePlots <-  FALSE
+closePlots <- FALSE
+
+if(savePlots) closePlots <- TRUE
 
 # Response variable y = brwt = brain weight
 # Covariate x = bwt = body weight
@@ -19,8 +22,8 @@ head(sleeptab)
 # 1.1
 # Plot response v.s.covariate
 if (savePlots) png("figures/Ex1_1.png")
-plot(bwt, brwt)
-graphics.off()
+plot(bwt, brwt, main = "Exercise 1.1")
+if (closePlots) if (closePlots) graphics.off()
 
 # Identify which species the three outliers belong to
 tooLarge <- which(brwt>1000) # Get indexes of the extreme values
@@ -34,8 +37,8 @@ brwt[tooLarge] # Brain weight in grams:
 # 1.2
 # Plot reponse without the outliers
 if (savePlots) png("figures/Ex1_2.png")
-plot(bwt[-tooLarge], brwt[-tooLarge]) #Exclude the indexes of the extreme values
-graphics.off()
+plot(bwt[-tooLarge], brwt[-tooLarge], main = "Exercise 1.2") #Exclude the indexes of the extreme values
+if (closePlots) if (closePlots) graphics.off()
 # Very linear for the smallest values, but less so for the largest.
 # An increase in body weight does seem to increase the brain weight.
 # TODO: Check, elaborate?
@@ -57,11 +60,12 @@ b0 = mean(brwt) - b1*mean(bwt)
 sleepModel <- lm(brwt~bwt)
 summary(sleepModel)
 if (savePlots) png("figures/Ex1_3.png")
-plot(bwt, brwt)
+plot(bwt, brwt, main = "Exercise 1.3")
 abline(sleepModel)
-graphics.off()
+if (closePlots) graphics.off()
 
-cat("Manual calc: \n", b0, b1,
+cat("Exercise 1.3:",
+    "\nManual calc: \n", b0, b1,
     "\nUsing ´lm´:\n", sleepModel$coefficients)
 # We get the same result when doing it manually and using the function, 
 # which is good! 
@@ -81,9 +85,9 @@ cat("Manual calc: \n", b0, b1,
 # 2.1 
 # Plot the residuals. Observe any extreme observation? Extreme by how much?
 if (savePlots) png("figures/Ex2_1.png")
-plot(sleepModel$residuals)
+plot(sleepModel$residuals, main = "Exercise 2.1")
 abline(h=0, col = "red")
-graphics.off()
+if (closePlots) graphics.off()
 sleepModel$residuals[abs(sleepModel$residuals)>500]
 #         1         5        34 
 # -810.0712 2050.3294 1169.0728 
@@ -98,16 +102,15 @@ sleepModel$residuals[abs(sleepModel$residuals)>500]
 logBwt <- log(bwt)   # Feels unnecessary to do this,
 logBrwt <- log(brwt) # but the code was easier to read this way
 
-if (savePlots) png("figures/Ex2_2.png")
-plot(logBwt, logBrwt)
-graphics.off()
+plot(logBwt, logBrwt, main = "Exercise 2.2")
+if (closePlots) graphics.off()
 
 logSleepModel <- lm(logBrwt~logBwt)
 
 if (savePlots) png("figures/Ex2_2.png")
-plot(logBwt, logBrwt)
+plot(logBwt, logBrwt, main = "Exercise 2.2")
 abline(logSleepModel)
-graphics.off()
+if (closePlots) graphics.off()
 # TODO: Check, elaborate?
 # [ ] Better, no? -> Better, yes!
 
@@ -165,12 +168,14 @@ Interval_b1 <- b1 + t_alpha*s / sqrt(sum((bwt-mean(bwt))^2))
 # TODO: Choose one printer:
 confint(sleepModel, level = 0.9)
 #cat(Interval_b0, "\n", Interval_b1)
-cat("\n\n\t\t", "5%\t\t95%", 
+cat("\n\nExercise 3.2:\n")
+cat("\t\t", "5%\t\t95%", 
     "\n(Intercept)\t", Interval_b0[1], "\t", Interval_b0[2], 
     "\nbwt\t\t", Interval_b1[1], "\t", Interval_b1[2], sep = '')
 
+cat("\n\nExercise 3.2:\n")
 cat(format(c(
-  "\n\n", "5%", "95%", 
+  "", "5%", "95%", 
   "b0, manual", signif(Interval_b0, 7), 
   "b0, code", signif(confint(sleepModel, level = 0.90)[1,], 7), 
   "b1, manual", signif(Interval_b1, 7), 
@@ -181,6 +186,76 @@ cat(format(c(
 # [x] Calculate the confidence intervals by hand, with confidence limit 0.9.
 # [x] You may not use `confint`, but may check your results with this.
 # [ ] Add your concrete interpretation of such intervals.
+
+
+# Exercise 4 ----
+
+# Denote the already obtained least squares estimates with b0_old and b1_old.
+# Lets pretend these are the true parameter values.
+
+# Write a for-loop to produce 2000 sets of parameter estimates and confidence
+# intervals.
+
+# Plug the b0_old and b1_old into the linear model and produce simulated
+# observations brwt_new(i) = b0_old + b1_old*bwt(i) + e_new(i), 
+# for i = 1, ..., 62 where bwt(i) is the same values as the original datasets.
+
+# Use s = 334.7198 (from Ex 3) with rnorm to produce e_new(i) ~ N(0, s^2)
+
+# Fit the simulated dataset, D_1(bwt(i), brwt_new(i)), i=1...n, and denote the
+# parameter estimates b0_hat(1) and b1_hat(1)
+
+# Repeat the procedure to create new observations brwt_new(i) and e_new(i) using
+# rnorm and call the new dataset D_2(bwt(i), brwt_new(i)), i=1...n. Fit D_2 via
+# linear regression and denote the parameter estimates b0_hat(2) and b1_hat(2).
+
+# Repeat until you have 2000 sets of estimates. Construct the confidence
+# intervals (1-alpha=0.9) from each set of estimates, so that you have 2000
+# intervals for b0_old and b1_old respectively.
+
+# Compute the proportion of intervals that include the original value b0_old and
+# b1_old respectively, and show that both proportions are very close to 1-alpha, as
+# expected from the theory.
+
+numEstimates <- 2000
+numObservations <- 62
+
+# From previous exercises
+b0_old <- sleepModel$coefficients[1]
+b1_old <- sleepModel$coefficients[2]
+s <- sigma(sleepModel)
+
+b0_new <- c()
+b1_new <- c()
+confint_b0 <- data.frame("low"=0, "high"=0)
+confint_b1 <- data.frame("low"=0, "high"=0)
+
+for (N in 1:numEstimates) {
+  # Create 62 error terms:
+  error <- rnorm(numObservations, mean = 0, sd = s^2) 
+  # [ ] Should s above be s^2?
+  
+  # Predict new brainweights using the existing body weights and the error terms
+  brwt_new <- b0_old + b1_old*bwt + error
+  newmodel <- lm(brwt_new ~ bwt)
+  
+  # Save the new parameters
+  b0_new[N] <- newmodel$coefficients[1] # Save them but just in case?
+  b1_new[N] <- newmodel$coefficients[2]
+  
+  # Calculate the confidence intervals
+  new_confint <- confint(newmodel, level = 0.9)
+  confint_b0[N,] <- new_confint[1, ] # Add to row N in the matrix
+  confint_b1[N,] <- new_confint[2, ]
+}
+# Count how many intervals includes the "true" value, i.e. count the ones where 
+# the statements are both true and sum to 2.
+includes_b0 <- sum((b0_old > confint_b0$low) + (b0_old < confint_b0$high)==2)
+includes_b1 <- sum((b1_old > confint_b1$low) + (b1_old < confint_b1$high)==2)
+cat("\nExercise 4:")
+cat("\nThe proportion of intervals which include b0_old is", includes_b0/N)
+cat("\nThe proportion of intervals which include b1_old is", includes_b1/N)
+
 
 detach(sleeptab)
 

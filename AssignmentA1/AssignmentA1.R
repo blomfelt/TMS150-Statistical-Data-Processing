f67c546c79ede6fbb36838fa614ec00b18e161b0
feloxes <155644281+feloxes@users.noreply.github.com>
2024-09-11 18:02:34 +0200

    Exercise 3 done

--- AssignmentA1/AssignmentA1.R
+++ AssignmentA1/AssignmentA1.R
@@ -1,5 +1,6 @@
-#setwd("~/Documents/RStudio/TMS150 - Stochastic Data Processing/AssignmentA1")
-setwd("~/Documents/TMS150-Statistical-Data-Processing/AssignmentA1")
+rm(list=ls())
+setwd("~/Documents/RStudio/TMS150 - Stochastic Data Processing/AssignmentA1")
+#setwd("~/Documents/TMS150-Statistical-Data-Processing/AssignmentA1")
 
 # To save all plots change to TRUE
 savePlots <-  FALSE
@@ -12,29 +13,35 @@ sleeptab <- read.table('sleeptab.dat',header=TRUE) # loads the dataset
 # the option header = TRUE tells R that the first row
 # contains the variables names
 attach(sleeptab) # this way you can access the variables by name
-# Inspect the data:
+# Inspect the data and show the structure of it:
 head(sleeptab)
 
 # 1.1
+# Plot response v.s.covariate
 if (savePlots) png("figures/Ex1_1.png")
 plot(bwt, brwt)
-dev.off()
+graphics.off()
 
-# 1.2
-tooLarge <- which(brwt>1000)
+# Identify which species the three outliers belong to
+tooLarge <- which(brwt>1000) # Get indexes of the extreme values
+  # 1000 is guessed from the plot
 Species_of_animal[tooLarge]
 # "Africanelephant" "Asianelephant" "Man"
-brwt[tooLarge]
+brwt[tooLarge] # Brain weight in grams:
 # 5712 4603 1320
+# TODO: Check, elaborate?
 
+# 1.2
+# Plot reponse without the outliers
 if (savePlots) png("figures/Ex1_2.png")
-plot(bwt[-tooLarge], brwt[-tooLarge])
-dev.off()
+plot(bwt[-tooLarge], brwt[-tooLarge]) #Exclude the indexes of the extreme values
+graphics.off()
 # Very linear for the smallest values, but less so for the largest.
 # An increase in body weight does seem to increase the brain weight.
+# TODO: Check, elaborate?
 
 # 1.3 
-# Linear fit by hand
+# Compute a linear fit by hand
 # x = bwt
 # y = brwt
 
@@ -49,47 +56,60 @@ b0 = mean(brwt) - b1*mean(bwt)
 # Linear fit by 'lm'
 sleepModel <- lm(brwt~bwt)
 summary(sleepModel)
+if (savePlots) png("figures/Ex1_3.png")
 plot(bwt, brwt)
 abline(sleepModel)
+graphics.off()
 
 cat("Manual calc: \n", b0, b1,
     "\nUsing ´lm´:\n", sleepModel$coefficients)
 # We get the same result when doing it manually and using the function, 
 # which is good! 
 # BUT using the function seems exponentially easier the more variables you have
-
+# TODO: Check, elaborate?
 
 # 1.4 
+# Interpret the value b1. What can you say about it? And what does it help you 
+# conclude about the increase in brain weight for a 100 kg increase in bwt?
+
 # The value b1 represent the slope, so an increase of 1 kg body weight correlate
 # with an increase of 0.97 g brwt. This means that an 100 kg increase in body
 # weight would lead, in average, to a 96 grams increase of brain weight.
+# TODO: Check, elaborate?
 
 # Exercise 2 ----
-# You may use the predict() function
 # 2.1 
+# Plot the residuals. Observe any extreme observation? Extreme by how much?
 if (savePlots) png("figures/Ex2_1.png")
 plot(sleepModel$residuals)
 abline(h=0, col = "red")
-dev.off()
+graphics.off()
 sleepModel$residuals[abs(sleepModel$residuals)>500]
 #         1         5        34 
 # -810.0712 2050.3294 1169.0728 
 # The same three as before
+# TODO: Check, elaborate?
+# [ ] Do you observe any extreme observation? 
+# [ ] Extreme by how much?
 
 # 2.2
-logBwt <- log(bwt)
-logBrwt <- log(brwt)
+# Log-transform both variables, and plot them. Perform regression using the
+# transformed variables, then plot it.
+logBwt <- log(bwt)   # Feels unnecessary to do this,
+logBrwt <- log(brwt) # but the code was easier to read this way
 
 if (savePlots) png("figures/Ex2_2.png")
 plot(logBwt, logBrwt)
-dev.off()
+graphics.off()
 
 logSleepModel <- lm(logBrwt~logBwt)
 
 if (savePlots) png("figures/Ex2_2.png")
 plot(logBwt, logBrwt)
 abline(logSleepModel)
-dev.off()
+graphics.off()
+# TODO: Check, elaborate?
+# [ ] Better, no? -> Better, yes!
 
 # 2.3
 # Estimate the value of brain weight when the body weight is 1000 kg using both
@@ -97,7 +117,7 @@ dev.off()
 # to exponentiate the result. Also give the difference between them.
 # Comment on whether you think this is substantial. 
 
-#First model
+# First model
 predict(sleepModel, newdata = data.frame(bwt=1000))
 # 1057.501 
 # Transformed model
@@ -107,15 +127,61 @@ exp(predict(logSleepModel, newdata = data.frame(logBwt = log(1000))))
 # 1521.195 - 1057.501 = 463.694
 # 463.694 / 1057.501 = 0.4384809
 
-# COMMENT TODO
+# TODO: Check, elaborate?
 
 
-# Estimation for later
-x0 <- seq(min(bwt), max(bwt), length = 100) 
-E0 <- predict(sleepModel, newdata = data.frame(bwt = x0))
+# Exercise 3 ----
+# 3.1 
+# Calculate the value of s "by hand", check using confint().
+# Formula:
+#           sum( (y(i) - estimate(y))^2 )
+# s = sqrt( ------------------------- )
+#                   (n - 2)
 
-# now produce plots of the estimated m2, by predicting from a grid of x0 values defined as below
-# estimate E(y) according to x0
-E0 <- predict(sleepModel, newdata = data.frame(bwt = x0))  # estimated expected distance using model m2 based on speed set at x0 
-# add the predictions we have just obtained to the plot with data
-lines(x0, E0, col = "red")  # add regression curve (colour: red)
+n <- length(brwt)
+
+# Use the body weight (bwt) to predict new response and calculate the residual
+residualsManual <- brwt-predict(sleepModel, newdata = data.frame(bwt))
+# Use the formula for s, see above, to calculate it
+s <- sqrt(sum(residualsManual^2)/(n - 2))
+# 334.7198
+# TODO: elaborate? "The value of s is 334.7198."??
+
+# 3.2 
+# Calculate "by hand" the confidence interval of b0 and b1 using a confidence 
+# level of 0.9.
+
+# The confidence limit 1-alpha = 0.9 gives alpha = 0.1, which means that 
+# alpha/2 = 0.05 and 1-alpha/2 = 0.95
+# This gives 
+n <-  length(bwt)
+t_alpha <-  qt(c(0.05, 0.95), n-2)
+b0 <- sleepModel$coefficients[1]
+b1 <- sleepModel$coefficients[2]
+
+Interval_b0 <- b0 + t_alpha*s*sqrt(1/n + mean(bwt)^2 / sum((bwt-mean(bwt))^2))
+Interval_b1 <- b1 + t_alpha*s / sqrt(sum((bwt-mean(bwt))^2))
+
+# TODO: Choose one printer:
+confint(sleepModel, level = 0.9)
+#cat(Interval_b0, "\n", Interval_b1)
+cat("\n\n\t\t", "5%\t\t95%", 
+    "\n(Intercept)\t", Interval_b0[1], "\t", Interval_b0[2], 
+    "\nbwt\t\t", Interval_b1[1], "\t", Interval_b1[2], sep = '')
+
+cat(format(c(
+  "\n\n", "5%", "95%", 
+  "b0, manual", signif(Interval_b0, 7), 
+  "b0, code", signif(confint(sleepModel, level = 0.90)[1,], 7), 
+  "b1, manual", signif(Interval_b1, 7), 
+  "b1, code", signif(confint(sleepModel, level = 0.90)[2,], 7)), 
+  justify = 'right'), fill = 40)
+
+# TODO: 
+# [x] Calculate the confidence intervals by hand, with confidence limit 0.9.
+# [x] You may not use `confint`, but may check your results with this.
+# [ ] Add your concrete interpretation of such intervals.
+
+detach(sleeptab)
+
+
